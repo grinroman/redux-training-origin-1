2481ea6deaada3fb83f7522d42e4dbba7bc50d02
Roman Grin <grinromchik@gmail.com>
2022-09-14 23:39:12 +0300

    react-redux hooks implemented

--- src/index.js
+++ src/index.js
@@ -1,64 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { createStore } from 'redux';
-// import App from './App';
-
-const initialState = { value: 0 };
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INC':
-            return { ...state, value: state.value + 1 };
-        case 'DEC':
-            return { ...state, value: state.value - 1 };
-        case 'RND':
-            return { ...state, value: state.value * action.payload };
-        default:
-            return state;
-    }
-};
-
-const store = createStore(reducer);
-const inc = () => ({
-    type: 'INC',
-});
-
-const dec = () => ({
-    type: 'DEC',
-});
-
-const rnd = (value) => ({
-    type: 'RND',
-    payload: value,
-});
-
-const update = () => {
-    document.getElementById('counter').textContent = store.getState().value;
-};
-
-store.subscribe(update);
-
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
-});
-
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
-});
-
-document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
-});
-
-// let state = reducer(initialState, { type: 'INC' });
-// state = reducer(state, { type: 'INC' });
-// state = reducer(state, { type: 'INC' });
+import reducer from './reducer';
+import App from './components/App';
+import { Provider } from 'react-redux';
+// import { dec, inc, rnd } from './actions';!!!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 root.render(
     <React.StrictMode>
-        <></>
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
+// update();  // провайдер уже обеспечивает изменение нашего VirtualDOM
+//subscribe(update); ///и обеспечивает изменение стейта в случае вызова dispatch
